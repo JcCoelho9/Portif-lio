213ac21f554d19589c26a38e0256377d4261bef9
Coelho <juliocesar.tdeandrade@protonmail.com>
2024-07-10 14:52:54 -0300

    Terceira rodada de commits. Ainda na responsividade

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,34 +2,30 @@ import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import { useState } from 'react';
 
-function Header(){
+function Header() {
     const [showMenu, setShowMenu] = useState(false);
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     };
-    return(
+
+    return (
         <header className={styles.header}>
             <Link to="/">
                 <span>Coelho.dev</span>
             </Link>
-            <nav 
-                className={`${styles.menuSandwich} ${ showMenu ? styles.show : '' }`}
-                onClick={toggleMenu}
-            >
-                <Link to="/" >Home</Link>
-                <Link to="/sobre" >Sobre</Link>
-                <Link to="/projetos" >Projetos</Link>
-                <Link to="/contatos" >Contatos</Link>
+            <nav className={`${styles.menuSandwich} ${showMenu ? styles.show : ''}`}>
+                <Link to="/" onClick={toggleMenu}>Home</Link>
+                <Link to="/sobre" onClick={toggleMenu}>Sobre</Link>
+                <Link to="/projetos" onClick={toggleMenu}>Projetos</Link>
+                <Link to="/contatos" onClick={toggleMenu}>Contatos</Link>
             </nav>
-            <div 
-                className={styles.menuButton}
-                onClick={toggleMenu}
-            >
-                <span className={styles.linha}></span>    
-                <span className={styles.linha}></span>    
-                <span className={styles.linha}></span>    
+            <div className={styles.menuButton} onClick={toggleMenu}>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
             </div>
         </header>
     );
-} 
+}
+
 export default Header;
--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -3,15 +3,15 @@ import styles from './Page404.module.css'
 
 function Page404(){
     return(
-        <>
+        <section className={styles.page404}>
             
-                    <h2 className={styles.titulo2}>Há algo de errado que não está certo...</h2>
+                <h2 className={styles.titulo2}>Há algo de errado que não está certo...</h2>
                     <div className={styles.textos}>
                         <span className={styles.texto_grande}>404</span><br />
                         <strong className={styles.texto_vermelho}>Página não localizada!</strong>
                     </div>
                 
-        </>
+        </section>
     )
 }
 
