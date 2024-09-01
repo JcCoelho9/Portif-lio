009b3813ef4b3846eb574134c2d719cfc3057086
Coelho <juliocesar.tdeandrade@protonmail.com>
2024-09-01 18:58:52 -0300

    Alterações nos ícones de tecnologia e renomeando para Portifolio

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,29 +1,58 @@
-import styles from './Card.module.css'
-import { FaCss3Alt, FaHtml5, FaJs, FaReact } from "react-icons/fa";
-import { BsArrowRight } from "react-icons/bs";
+import styles from './Card.module.css';
+import { FaCss3Alt, FaHtml5, FaJs, FaReact, FaNode, FaPhp } from 'react-icons/fa';
+import { FaBootstrap } from "react-icons/fa6";
+import { BsArrowRight } from 'react-icons/bs';
+import { SiMysql, SiCisco } from 'react-icons/si';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { SiVite } from "react-icons/si";
 
+function Card({ name, description, html_url, languages }) {
+  // Função para mapear as linguagens para os ícones correspondentes
+  const renderLanguageIcons = () => {
+    return languages.map((language) => {
+      switch (language.toLowerCase()) {
+        case 'html':
+          return <FaHtml5 key={language} />;
+        case 'css':
+          return <FaCss3Alt key={language} />;
+        case 'javascript':
+          return <FaJs key={language} />;
+        case 'react':
+          return <FaReact key={language} />;
+        case 'php':
+          return <FaPhp key={language} />;
+        case 'nodejs':
+          return <FaNode key={language} />;
+        case 'mysql':
+          return <SiMysql key={language} />;
+        case 'cisco':
+          return <SiCisco key={language} />;
+        case 'bootstrap':
+          return <FaBootstrap key={language} />;
+        case 'vite':
+          return <SiVite key={language} />; 
+        case 'search-envelope':
+          return <AiOutlineSearch key={language} />;
+        default:
+          return null;
+      }
+    });
+  };
 
-// eslint-disable-next-line react/prop-types
-function Card({ name, description, html_url }){
-    return(
-        <section className={styles.card}>
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <div className={styles.card_footer}>
-                <div className={styles.card_icones}>
-                    <FaHtml5/> 
-                    <FaCss3Alt/> 
-                    <FaJs/> 
-                    <FaReact/> 
-
-                </div>
-                <a href={html_url}  className={styles.botao} target="_blank">
-                <BsArrowRight />
-                </a>
-            </div>
-            
-        </section>
-    )
+  return (
+    <section className={styles.card}>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <div className={styles.card_footer}>
+        <div className={styles.card_icones}>
+          {renderLanguageIcons()}
+        </div>
+        <a href={html_url} className={styles.botao} target="_blank" rel="noopener noreferrer">
+          <BsArrowRight />
+        </a>
+      </div>
+    </section>
+  );
 }
 
-export default Card
+export default Card;
--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,43 +1,79 @@
 import { useEffect, useState } from "react";
-import Card from "../../components/Card"
-import styles from "./Projetos.module.css"
+import Card from "../../components/Card";
+import styles from "./Projetos.module.css";
 
 function Projetos() {
+    const [repositories, setRepositories] = useState([]);
 
-    const [repositories, setRepositories] = useState([])
-
-    useEffect(() =>{
+    useEffect(() => {
         const buscarRepositorios = async () => {
-            const response = await fetch('https://api.github.com/users/JcCoelho9/repos?page=1&per_page=50')
-            const data = await response.json()
-            setRepositories(data)
-        }
-        buscarRepositorios()
-    },[])
+            const response = await fetch('https://api.github.com/users/JcCoelho9/repos?page=1&per_page=50');
+            const data = await response.json();
+
+            // Definindo tecnologias específicas para cada repositório
+            const reposWithLanguages = data.map(repo => {
+                let languages = [];
+                switch (repo.name.toLowerCase()) {
+                    case 'api':
+                        languages = ['nodejs', 'javascript'];
+                        break;
+                    case 'cicle':
+                        languages = ['mysql'];
+                        break;
+                    case 'crud':
+                        languages = ['php', 'mysql', 'bootstrap']; // Adicione 'bootstrap' aqui
+                        break;
+                    case 'jogo-da-memoria':
+                        languages = ['react', 'javascript', 'css', 'html'];
+                        break;
+                    case 'portif-lio':  // Corrigido para o nome correto
+                        languages = ['react', 'javascript', 'css', 'html', 'vite'];
+                        break;
+                    case 'redes':
+                        languages = ['cisco'];
+                        break;
+                    case 'trabalhos-html-css':
+                        languages = ['bootstrap', 'css', 'html'];
+                        break;
+                    default:
+                        languages = [];
+                        break;
+                }
+                return {
+                    ...repo,
+                    languages
+                };
+            });
+
+            setRepositories(reposWithLanguages);
+        };
+
+        buscarRepositorios();
+    }, []);
 
     return (
-        <section className={styles.projetos}>            
+        <section className={styles.projetos}>
             <h2>Projetos</h2>
             {
                 repositories.length > 0 ? (
-                   <section className={styles.lista}> 
-                   {
-                        repositories.map((repo) =>(
-                            <Card key={repo.id} 
-                                name={repo.name} 
-                                description={repo.description} 
-                                html_url={repo.html_url}
-                            />
-                     ))
-                   }
+                    <section className={styles.lista}>
+                        {
+                            repositories.map((repo) => (
+                                <Card key={repo.id}
+                                    name={repo.name}
+                                    description={repo.description}
+                                    html_url={repo.html_url}
+                                    languages={repo.languages}
+                                />
+                            ))
+                        }
                     </section>
-                    
                 ) : (
                     <p>Carregando repositórios...</p>
                 )
             }
         </section>
-    )
+    );
 }
 
 export default Projetos;
